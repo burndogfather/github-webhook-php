7f6175bd25fd2c93db615535a39f920d23cd79a0
coco <squarehacker@kakao.com>
2022-10-26 02:27:21 +0900

    2022-10-26 02:27:21 업데이트!!!

---
+++ @.setting.php
@@ -0,0 +1,8 @@
+<?php
+	//깃허브 설정
+	define('__GITHUB_API__','https://api.github.com');
+	define('__GITHUB_OWNER__','squarehacker'); //깃허브 소유주
+	define('__GITHUB_TOKEN__','ghp_blabla'); //프로필 > Setting > Developer settings > Personal access tokens > Tokens(classic)에서 발급받은 코드
+	define('__GITHUB_REPO__','github-webhook-php'); //리포지토리 명칭
+	define('__GITHUB_WH_SECRET__','oiawjdoiuH&*(@#Y$*&Yhgr8giyuher234'); //리포지토리 > Settings > Webhooks에서 Secret key를 입력
+?>
